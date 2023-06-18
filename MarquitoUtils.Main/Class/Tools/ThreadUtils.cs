b1cb494d4a3361b932ae197e98b916b928203135
marquito73 <theobegyn@live.com>
2023-06-18 14:45:37 +0200

    .

---
+++ MarquitoUtils.Main/Class/Tools/ThreadUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarquitoUtils.Main.Class.Tools
+{
+    /// <summary>
+    /// Utils class for Threading
+    /// </summary>
+    public class ThreadUtils
+    {
+        /// <summary>
+        /// Get a cyclic thread, ready to start
+        /// </summary>
+        /// <param name="millisecondsDelay">Delay in milliseconds, 
+        /// on which the thread make a cycle, as long as he isn't aborted</param>
+        /// <param name="threadAction">The code to execute for this thread</param>
+        /// <returns></returns>
+        public static Thread GetCyclicThread(int millisecondsDelay,
+            Action threadAction)
+        {
+            return new Thread(() =>
+            {
+                bool threadIsOk = true;
+
+                while (threadIsOk)
+                {
+                    try
+                    {
+                        // Make action for the thrad
+                        threadAction();
+                        // This thread is in loop, cadenced by specific time
+                        Thread.Sleep(millisecondsDelay);
+                    } catch (Exception ex)
+                    {
+                        // We go here when we ask to thread to stop
+                        threadIsOk = false;
+                    }
+                }
+            });
+        }
+    }
+}
