25a0b70b6af8f0c2245c35cf1421c8b543a34e4a
theo begyn <theobegyn@live.com>
2023-10-14 15:38:07 +0200

    Add new class for Translation

---
+++ MarquitoUtils.Main/Class/Translations/Translate.cs
@@ -0,0 +1,29 @@
+﻿using MarquitoUtils.Main.Class.Entities.Translation;
+using MarquitoUtils.Main.Class.Service.General;
+using MarquitoUtils.Main.Class.Tools;
+
+namespace MarquitoUtils.Main.Class.Translations
+{
+    /// <summary>
+    /// Static class for store translations
+    /// </summary>
+    public static class Translate
+    {
+        /// <summary>
+        /// The service for the translations
+        /// </summary>
+        public static ITranslateService TranslateService { get; private set; }
+
+        /// <summary>
+        /// Set translations (don't work if translations are already set
+        /// </summary>
+        /// <param name="translations"></param>
+        public static void SetTranslationService(List<Translation> translations)
+        {
+            if (Utils.IsNull(TranslateService))
+            {
+                TranslateService = new TranslateService(translations);
+            }
+        }
+    }
+}
