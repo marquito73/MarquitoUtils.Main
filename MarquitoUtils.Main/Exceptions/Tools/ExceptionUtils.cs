b0b9ba6b7704209e23329c0158f8b65d0fa13ec6
theo begyn <theobegyn@live.com>
2026-01-03 16:55:41 +0100

    Add utils method for get data inside his Data Dictionary

---
+++ MarquitoUtils.Main/Exceptions/Tools/ExceptionUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace MarquitoUtils.Main.Exceptions.Tools
+{
+    public static class ExceptionUtils
+    {
+        public static T GetData<T>(this Exception exception, string dataName)
+        {
+            return (T)exception.Data[dataName];
+        }
+    }
+}
