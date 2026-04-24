7a88478b80f2b0ef7e8cc3d98893754f37d7c0ce
theo begyn <theobegyn@live.com>
2026-04-24 09:43:19 +0200

    Add Json Converter for System.Drawing.Color <> string

---
+++ MarquitoUtils.Main/Common/Converters/ColorJsonConverter.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Drawing;
+
+namespace MarquitoUtils.Main.Common.Converters
+{
+    /// <summary>
+    /// A convertor, for convert string to <see cref="Color"/> and vice versa, for json serialization and deserialization.
+    /// </summary>
+    public class ColorJsonConverter : JsonConverter<Color>
+    {
+        public override Color ReadJson(JsonReader reader, Type objectType, Color existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            JToken token = JToken.Load(reader);
+
+            string s = token.Type == JTokenType.String ? token.ToString() : "#000000";
+
+            return ColorTranslator.FromHtml(s);
+        }
+
+        public override void WriteJson(JsonWriter writer, Color value, JsonSerializer serializer)
+        {
+            Color color = value;
+
+            writer.WriteValue(ColorTranslator.ToHtml(color));
+        }
+    }
+}
